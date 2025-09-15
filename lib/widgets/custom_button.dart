1f393a5276865b6559455d6ee33b65d573007cac
HushABF <nkbh12342@gmail.com>
2025-09-15 09:33:02 +0300

    used bloc builder and consumer

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   final Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,11 +17,20 @@ class CustomButton extends StatelessWidget {
         ),
         width: MediaQuery.of(context).size.width,
         height: 40,
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-          ),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: CircularProgressIndicator(color: Colors.black),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
